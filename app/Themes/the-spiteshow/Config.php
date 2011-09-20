2aaf3486debc36a24125f647aa76f301adc9c9d3
TJ Eastmond <tj.eastmond@gmail.com>
2011-09-20 12:38:22 -0400

    removing personal theme

--- app/Themes/the-spiteshow/Config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Registry::set('theme_options',array(
-	'enable_twitter' => array('default' => '0', 'input' => 'text'),
-	'number_of_tweets_to_display' => array('default' => '3', 'input' => 'text')
-));
